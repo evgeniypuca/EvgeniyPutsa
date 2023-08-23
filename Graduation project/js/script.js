a9cf65e34b271c1c8e65d122d953afe2e4db5eda
EvgeniyPuca <epuca3285@gmail.com>
2023-08-23 13:52:02 +0300

    32423

--- Graduation project/js/script.js
+++ Graduation project/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider1').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -7,148 +7,171 @@ $(document).ready(function(){
         speed: 2000,
         // fade: true,
     });
-    
+
     $('.slider2').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    // autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    dots:true,
-    responsive:[
-        {
-            breakpoint:1100,
-            settings:{
-                slidesToShow: 3,
-                slidesToScroll: 3 
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                },
+
             },
-            
-        },
-        {
-            breakpoint:850,
-            settings:{
-                slidesToShow: 2,
-                slidesToScroll: 2 
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                },
+
             },
-            
-        },
-        {
-            breakpoint:650,
-            settings:{
-                slidesToShow: 1,
-                slidesToScroll: 1 
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+
+            }
+        ]
+    });
+    $('.slider3').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                },
+
             },
-            
-        }
-    ]
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+
+            }
+        ]
+    });
+    $('.slider4').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                },
+
+            },
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                },
+
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+
+            }
+        ]
+    });
+
+    $('.slider5').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                },
+
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                },
+
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+
+            }
+        ]
+    });
+
 });
-$('.slider3').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    // autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    dots:true,
-    responsive:[
-        {
-            breakpoint:1100,
-            settings:{
-                slidesToShow: 2,
-                slidesToScroll: 2 
-            },
-            
-        },
-        {
-            breakpoint:650,
-            settings:{
-                slidesToShow: 1,
-                slidesToScroll: 1 
-            },
-            
-        }
-    ]
-});   
-$('.slider4').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    dots:true,
-    arrows:false,
-    responsive:[
-        {
-            breakpoint:1200,
-            settings:{
-                slidesToShow: 3,
-                slidesToScroll: 3 
-            },
-            
-        },
-        {
-            breakpoint:950,
-            settings:{
-                slidesToShow: 2,
-                slidesToScroll: 2 
-            },
-            
-        },
-        {
-            breakpoint:650,
-            settings:{
-                slidesToShow: 1,
-                slidesToScroll: 1 
-            },
-            
-        }
-    ]
-});  
-
-$('.slider5').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    dots:true,
-    arrows:false,
-    responsive:[
-        {
-            breakpoint:1100,
-            settings:{
-                slidesToShow: 3,
-                slidesToScroll: 3 
-            },
-            
-        },
-        {
-            breakpoint:850,
-            settings:{
-                slidesToShow: 2,
-                slidesToScroll: 2 
-            },
-            
-        },
-        {
-            breakpoint:650,
-            settings:{
-                slidesToShow: 1,
-                slidesToScroll: 1 
-            },
-            
-        }
-    ]
-});  
-
-}); 
 
 function dropList(number) {
     document.getElementById("Drop" + number).classList.toggle("show");
-  };
+};
 
-  var menuBtn = document.querySelector('.menu-btn');
-  var menu = document.querySelector('.header-bottom__menu');
-  menuBtn.addEventListener('click', function(){
-      menuBtn.classList.toggle('active');
-      menu.classList.toggle('active');
-  });
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.header-bottom__menu');
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});
 
 
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function () {
+
+        $('body,html').animate({ scrollTop: 0 }, 800);
+
+    });
+
+});
